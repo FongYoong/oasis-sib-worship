dc7368b94d0591d1e812f75acce6a6f6f9e89349
Fong Yoong <fongyoong8@gmail.com>
2022-01-27 10:56:55 +0800

    Fixed prisma import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,24 @@
 import { PrismaClient } from '@prisma/client'
 
 //let prisma : PrismaClient
-declare let prisma: PrismaClient;
+//declare let prisma: PrismaClient;
 declare global {
     // eslint-disable-next-line no-var
     var prisma: PrismaClient
 }
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-} else {
-    if (!globalThis.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
+const prisma = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV === "development"){
+    global.prisma = prisma;
 }
+
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient()
+// } else {
+//     if (!globalThis.prisma) {
+//         global.prisma = new PrismaClient()
+//     }
+//     prisma = global.prisma
+// }
 
 export default prisma
