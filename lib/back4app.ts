dd0eed4ec4dc60b6dc59dc76d3f8bf9cb0fd3f14
Fong Yoong <fongyoong8@gmail.com>
2022-01-31 02:06:37 +0800

    Removed Back4App

--- lib/back4app.ts
+++
@@ -1,6 +0,0 @@
-import Parse from 'parse/node'
-
-Parse.initialize(process.env.BACK4APP_APP_ID, process.env.BACK4APP_JS_KEY);
-Parse.serverURL = 'https://parseapi.back4app.com/'
-
-export { Parse };
